9b0219ead5a8b5a7975c1145ecefccf275ee3bfd
RachidNaitSaid2002 <rachidnaitsaid15@gmail.com>
2026-01-27 11:02:22 +0100

    Chunking text extracted on pdf

--- scripts/LoadPDF.py
+++ scripts/LoadPDF.py
@@ -1,21 +1,25 @@
 from langchain_community.document_loaders import PyPDFLoader
 import pprint
 
-loader = PyPDFLoader("./Data/Data.pdf")
+# Load Pdf
+def LoadPdf(pdf_path : str):
+    loader = PyPDFLoader(pdf_path)
+    pages = loader.load()
+    # i = 4
+    # print(f"--- Page {i + 1} ---")
+    # print(f"Content Sample: {pages[i].page_content}") 
+    # print(f"Metadata:")
+    # pprint.pprint(pages[i].metadata)
+    # print("-" * 20)
+    # --------------------------------------------------------
+    # for i, page in enumerate(pages):
+    #     print(f"--- Page {i + 1} ---")
+    #     print(f"Content Sample: {page.page_content[:200]}...") 
+    #     print(f"Metadata:")
+    #     pprint.pprint(page.metadata)
+    #     print("-" * 20)
+    return pages
 
-pages = loader.load()
-i = 20
 
+pages = LoadPdf("./Data/Data.pdf")
 
-print(f"--- Page {i + 1} ---")
-print(f"Content Sample: {pages[i].page_content}") 
-print(f"Metadata:")
-pprint.pprint(pages[i].metadata)
-print("-" * 20)
-
-# for i, page in enumerate(pages):
-#     print(f"--- Page {i + 1} ---")
-#     print(f"Content Sample: {page.page_content[:200]}...") 
-#     print(f"Metadata:")
-#     pprint.pprint(page.metadata)
-#     print("-" * 20)
