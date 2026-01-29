27022ddcac0b09693513e653ee5fe19669b379c8
RachidNaitSaid2002 <rachidnaitsaid15@gmail.com>
2026-01-29 14:37:33 +0100

    Create Core Function That Connect all services functions

--- scripts/RetrievalQA.py
+++ scripts/RetrievalQA.py
@@ -4,18 +4,21 @@ from Model import Get_Model
 from Retrieve_Data import Get_Retriever
 from Pre_Prompt import Get_Prompt
 
-def Retrieval_QA(llm, retriever,prompt):
+def Retrieval_QA():
+    llm = Get_Model()
+    retriever = Get_Retriever()
+    prompt = Get_Prompt()
     chain_qa = RetrievalQA.from_chain_type(
         llm=llm,
         retriever=retriever,
         chain_type="stuff",
-        chain_type_kwargs={'prompt':prompt}
+        chain_type_kwargs={'prompt':prompt},
+        return_source_documents=True
     )
     return chain_qa
 
 if __name__ == "__main__":
-    llm = Get_Model()
-    retriever = Get_Retriever()
-    prompt = Get_Prompt()
-    chain = Retrieval_QA(llm, retriever, prompt)
-    print(chain)
+    qa = Retrieval_QA()
+    question = "what is messi ?"
+    generated_text = qa(question)
+    print(generated_text)
