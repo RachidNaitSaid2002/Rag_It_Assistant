5bd884543dbfdf91d4b303ae3264cdf57e4b8a10
RachidNaitSaid2002 <rachidnaitsaid15@gmail.com>
2026-02-02 15:57:05 +0100

    Create Utils functions for embedding and label names

---
+++ app/utils/question_clustering.py
@@ -0,0 +1,13 @@
+from labe_name import Get_Name
+from embedding_function import get_embedding
+import joblib
+
+def Get_Cluster(question):
+    model = joblib.load("/media/rachid/d70e3dc6-74e7-4c87-96bc-e4c3689c979a/lmobrmij/Projects/Rag_It_Assistant/ml/Models/kmeans_model.pkl")
+    embedding = get_embedding(question)
+    prediction = model.predict([embedding])
+    cluster_id = Get_Name(prediction[0])
+    return cluster_id
+
+if __name__ == "__main__":
+    print(Get_Cluster("Qui est Lionel Messi ?"))
