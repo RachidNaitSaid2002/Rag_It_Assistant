8d98811c4dd0d88a853844f0ac182d0b289427ac
RachidNaitSaid2002 <rachidnaitsaid15@gmail.com>
2026-02-05 01:45:38 +0100

    Dockerization app (backend/db/mlflow)

--- app/utils/check_answer.py
+++ app/utils/check_answer.py
@@ -1,8 +1,17 @@
 import joblib
 from app.utils.embedding_function import get_embedding
+import os
 
 def Check_Answer(question):
-    model = joblib.load("/media/rachid/d70e3dc6-74e7-4c87-96bc-e4c3689c979a/lmobrmij/Projects/Rag_It_Assistant/ml/Models/answer_status_model_v2.pkl")
+    BASE_RIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    model_path = os.path.join(BASE_RIR, 'ml', 'Models', 'answer_status_model_v2.pkl')
+    print(f"Model path: {model_path}", "*"*50)
+    try:
+        model = joblib.load(model_path)
+    except FileNotFoundError:
+        print(f"Error: Model file not found at {model_path}")
+        return None
+
     embedding = get_embedding(question)
     prediction = model.predict([embedding])
     return prediction[0]
